7dac809bb58db2a113d88b95ab7f0a2b5a2ccd06
Jacob Birkett <jacob@birkett.dev>
2022-01-18 17:30:41 -0700

    Implement conversions

--- src/lib.rs
+++ src/lib.rs
@@ -65,42 +65,12 @@ impl Color {
         }
     }
 
-    // fn neighboring(c: f64, x: f64, h1: f64) -> (f64, f64, f64) {
-    //     match () {
-    //         _ if 0.0 <= h1 && h1 < 1.0 => (c, x, 0.0),
-    //         _ if 1.0 <= h1 && h1 < 2.0 => (x, c, 0.0),
-    //         _ if 2.0 <= h1 && h1 < 3.0 => (0.0, c, x),
-    //         _ if 3.0 <= h1 && h1 < 4.0 => (0.0, x, c),
-    //         _ if 4.0 <= h1 && h1 < 5.0 => (x, 0.0, c),
-    //         _ if 5.0 <= h1 && h1 < 6.0 => (c, 0.0, x),
-    //         _ => (0.0, 0.0, 0.0),
-    //     }
-    // }
-
     // pub fn to_rgba(&self) -> Self {
     //     match self {
     //         Color::Rgba { .. } => *self,
     //         Color::Hsva { h, s, v, a } => {
-    //             //https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
-    //             let c = v * s;
-    //             let h1 = h / 60.0;
-    //             let x = c * (1.0 - (h1 % 2.0 - 1.0).abs());
-    //             let (r1, g1, b1) = Self::neighboring(c, x, h1);
-    //             let m = v - c;
-    //             let (r, g, b) = (r1 + m, g1 + m, b1 + m);
-
-    //             Color::Rgba { r, g, b, a: *a }
     //         }
     //         Color::Hsla { h, s, l, a } => {
-    //             // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
-    //             let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
-    //             let h1 = h / 60.0;
-    //             let x = c * (1.0 - (h1 % 2.0 - 1.0).abs());
-    //             let (r1, g1, b1) = Self::neighboring(c, x, h1);
-    //             let m = l - (c / 2.0);
-    //             let (r, g, b) = (r1 + m, g1 + m, b1 + m);
-
-    //             Color::Rgba { r, g, b, a: *a }
     //         }
     //     }
     // }
@@ -108,47 +78,9 @@ impl Color {
     // pub fn to_hsva(&self) -> Self {
     //     match self {
     //         Color::Rgba { r, g, b, a } => {
-    //             // https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
-    //             let xmax = r.max(g.max(*b));
-    //             let xmin = r.min(g.min(*b));
-    //             let c = xmax - xmin;
-    //             let mut h = match () {
-    //                 _ if c == 0.0 => 0.0,
-    //                 _ if xmax == *r => 60.0 * ((g - b) / c),
-    //                 _ if xmax == *g => 60.0 * ((b - r) / c + 2.0),
-    //                 _ if xmax == *b => 60.0 * ((r - g) / c + 4.0),
-    //                 _ => panic!(),
-    //             };
-    //             if h < 0.0 {
-    //                 h += 360.0
-    //             };
-    //             let s = match () {
-    //                 _ if xmax == 0.0 => 0.0,
-    //                 _ => c / xmax,
-    //             };
-
-    //             Color::Hsva {
-    //                 h,
-    //                 s,
-    //                 v: xmax,
-    //                 a: *a,
-    //             }
     //         }
     //         Color::Hsva { .. } => *self,
     //         Color::Hsla { h, s, l, a } => {
-    //             // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_HSV
-    //             let v = l + s * l.min(1.0 - l);
-    //             let sv = match () {
-    //                 _ if v == 0.0 => 0.0,
-    //                 _ => 2.0 * (1.0 - l / v),
-    //             };
-
-    //             Color::Hsva {
-    //                 h: *h,
-    //                 s: sv,
-    //                 v,
-    //                 a: *a,
-    //             }
     //         }
     //     }
     // }
@@ -156,42 +88,8 @@ impl Color {
     // pub fn to_hsla(&self) -> Self {
     //     match self {
     //         Color::Rgba { r, g, b, a } => {
-    //             // https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
-    //             let xmax = r.max(g.max(*b));
-    //             let xmin = r.min(g.min(*b));
-    //             let c = xmax - xmin;
-    //             let mut h = match () {
-    //                 _ if c == 0.0 => 0.0,
-    //                 _ if xmax == *r => 60.0 * ((g - b) / c),
-    //                 _ if xmax == *g => 60.0 * ((b - r) / c + 2.0),
-    //                 _ if xmax == *b => 60.0 * ((r - g) / c + 4.0),
-    //                 _ => panic!(),
-    //             };
-    //             if h < 0.0 {
-    //                 h += 360.0
-    //             };
-    //             let l = (xmax + xmin) / 2.0;
-    //             let s = match () {
-    //                 _ if l == 0.0 || l == 1.0 => 0.0,
-    //                 _ => c / (1.0 - (2.0 * xmax - c - 1.0).abs()),
-    //             };
-
-    //             Color::Hsla { h, s, l, a: *a }
     //         }
     //         Color::Hsva { h, s, v, a } => {
-    //             // https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_HSL
-    //             let l = v * (1.0 - (s / 2.0));
-    //             let sl = match () {
-    //                 _ if l == 0.0 || l == 1.0 => 0.0,
-    //                 _ => 2.0 * (1.0 - l / v),
-    //             };
-
-    //             Color::Hsla {
-    //                 h: *h,
-    //                 s: sl,
-    //                 l,
-    //                 a: *a,
-    //             }
     //         }
     //         Color::Hsla { .. } => *self,
     //     }
--- src/types.rs
+++ src/types.rs
@@ -18,3 +18,122 @@ pub struct Hsl {
     pub s: f64,
     pub l: f64,
 }
+
+impl Rgb {
+    pub fn to_hsv(&self) -> Hsv {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
+        let xmax = self.r.max(self.g.max(self.b));
+        let xmin = self.r.min(self.g.min(self.b));
+        let c = xmax - xmin;
+        let mut h = match () {
+            _ if c == 0.0 => 0.0,
+            _ if xmax == self.r => 60.0 * ((self.g - self.b) / c),
+            _ if xmax == self.g => 60.0 * ((self.b - self.r) / c + 2.0),
+            _ if xmax == self.b => 60.0 * ((self.r - self.g) / c + 4.0),
+            _ => panic!(),
+        };
+        if h < 0.0 {
+            h += 360.0
+        };
+        let s = match () {
+            _ if xmax == 0.0 => 0.0,
+            _ => c / xmax,
+        };
+
+        Hsv { h, s, v: xmax }
+    }
+
+    pub fn to_hsl(&self) -> Hsl {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
+        let xmax = self.r.max(self.g.max(self.b));
+        let xmin = self.r.min(self.g.min(self.b));
+        let c = xmax - xmin;
+        let mut h = match () {
+            _ if c == 0.0 => 0.0,
+            _ if xmax == self.r => 60.0 * ((self.g - self.b) / c),
+            _ if xmax == self.g => 60.0 * ((self.b - self.r) / c + 2.0),
+            _ if xmax == self.b => 60.0 * ((self.r - self.g) / c + 4.0),
+            _ => panic!(),
+        };
+        if h < 0.0 {
+            h += 360.0
+        };
+        let l = (xmax + xmin) / 2.0;
+        let s = match () {
+            _ if l == 0.0 || l == 1.0 => 0.0,
+            _ => c / (1.0 - (2.0 * xmax - c - 1.0).abs()),
+        };
+
+        Hsl { h, s, l }
+    }
+}
+
+impl Hsv {
+    pub fn to_rgb(&self) -> Rgb {
+        //https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+        let c = self.v * self.s;
+        let h1 = self.h / 60.0;
+        let x = c * (1.0 - (h1 % 2.0 - 1.0).abs());
+        let (r1, g1, b1) = neighboring(c, x, h1);
+        let m = self.v - c;
+        let (r, g, b) = (r1 + m, g1 + m, b1 + m);
+
+        Rgb { r, g, b }
+    }
+
+    pub fn to_hsl(&self) -> Hsl {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_HSL
+        let l = self.v * (1.0 - (self.s / 2.0));
+        let sl = match () {
+            _ if l == 0.0 || l == 1.0 => 0.0,
+            _ => 2.0 * (1.0 - l / self.v),
+        };
+
+        Hsl {
+            h: self.h,
+            s: sl,
+            l,
+        }
+    }
+}
+
+impl Hsl {
+    pub fn to_rgb(&self) -> Rgb {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
+        let c = (1.0 - (2.0 * self.l - 1.0).abs()) * self.s;
+        let h1 = self.h / 60.0;
+        let x = c * (1.0 - (h1 % 2.0 - 1.0).abs());
+        let (r1, g1, b1) = neighboring(c, x, h1);
+        let m = self.l - (c / 2.0);
+        let (r, g, b) = (r1 + m, g1 + m, b1 + m);
+
+        Rgb { r, g, b }
+    }
+
+    pub fn to_hsv(&self) -> Hsv {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_HSV
+        let v = self.l + self.s * self.l.min(1.0 - self.l);
+        let sv = match () {
+            _ if v == 0.0 => 0.0,
+            _ => 2.0 * (1.0 - self.l / v),
+        };
+
+        Hsv {
+            h: self.h,
+            s: sv,
+            v,
+        }
+    }
+}
+
+fn neighboring(c: f64, x: f64, h1: f64) -> (f64, f64, f64) {
+    match () {
+        _ if (0.0..1.0).contains(&h1) => (c, x, 0.0),
+        _ if (1.0..2.0).contains(&h1) => (x, c, 0.0),
+        _ if (2.0..3.0).contains(&h1) => (0.0, c, x),
+        _ if (3.0..4.0).contains(&h1) => (0.0, x, c),
+        _ if (4.0..5.0).contains(&h1) => (x, 0.0, c),
+        _ if (5.0..6.0).contains(&h1) => (c, 0.0, x),
+        _ => (0.0, 0.0, 0.0),
+    }
+}
